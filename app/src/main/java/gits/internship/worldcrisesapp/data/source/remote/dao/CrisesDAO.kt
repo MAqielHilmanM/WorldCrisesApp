97c06278324a5ab8fcc68e5ada6c15336bedaac2
M Aqiel Hilman M <maqiel2600@gmail.com>
2018-06-07 08:49:55 +0700

    New Branch
    create new branch

--- app/src/main/java/gits/internship/worldcrisesapp/data/source/remote/dao/CrisesDAO.kt
+++ app/src/main/java/gits/internship/worldcrisesapp/data/source/remote/dao/CrisesDAO.kt
@@ -1,6 +1,9 @@
 package gits.internship.worldcrisesapp.data.source.remote.dao
 
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 open class CrisesDAO {
+    @SerializedName("_id")
+    @Expose 
 }
